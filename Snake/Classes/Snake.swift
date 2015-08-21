3d9f3f1b7155ad7834ea363cf00e731e45dcb69d
Draveness <stark.draven@gmail.com>
2015-08-21 16:51:16 +0800

    Added snake variable to string

---
+++ Snake/Classes/Snake.swift
@@ -0,0 +1,18 @@
+//
+//  Snake.swift
+//  Snake
+//
+//  Created by Draveness on 21/8/15.
+//  Copyright (c) 2015 Draveness. All rights reserved.
+//
+
+import UIKit
+
+class Snake {
+    
+    let string: String
+    
+    init(_ string: String) {
+        self.string = string
+    }
+}
