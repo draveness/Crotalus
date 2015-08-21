1850bc4af00fbfc6542d21a407de0a35f24d6019
Draveness <stark.draven@gmail.com>
2015-08-21 17:21:24 +0800

    Added basic match methods

--- Snake/Classes/Snake.swift
+++ Snake/Classes/Snake.swift
@@ -10,9 +10,49 @@ import UIKit
 
 class Snake {
     
-    let string: String
-    
-    init(_ string: String) {
+    let string: NSMutableAttributedString
+    private var ranges: [NSRange] = []
+
+    init(_ string: NSMutableAttributedString) {
         self.string = string
+        self.ranges.append(NSRangeFromString(self.string.string))
     }
+    
+    convenience init(_ string: NSAttributedString) {
+        self.init(NSMutableAttributedString(attributedString: string))
+    }
+    
+    convenience init(_ string: NSString) {
+        self.init(NSMutableAttributedString(string: string as String))
+    }
+    
+    convenience init(_ string: String) {
+        self.init(NSMutableAttributedString(string: string))
+    }
+    
+    var all: Snake {
+        get {
+            ranges.removeAll()
+            ranges.append(NSRangeFromString(self.string.string))
+            return self
+        }
+    }
+    
+    func range(from: Int, to: Int) -> Snake {
+        ranges.removeAll()
+        ranges.append(NSRange(location: from, length: to - from))
+        return self
+    }
+    func range(location: Int, length: Int) -> Snake {
+        ranges.removeAll()
+        ranges.append(NSRange(location: location, length: length))
+        return self
+    }
+    func range(range: NSRange) -> Snake {
+        ranges.removeAll()
+        ranges.append(range)
+        return self
+    }
+
+    
 }
