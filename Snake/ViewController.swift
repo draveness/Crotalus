061a0a055f31db41ddb9724ab902d5d2bd74225f
Draveness <stark.draven@gmail.com>
2015-08-21 16:36:48 +0800

    Initial Commit

---
+++ Snake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by Draveness on 21/8/15.
+//  Copyright (c) 2015 Draveness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
